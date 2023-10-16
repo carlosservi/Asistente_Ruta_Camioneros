e385f3d0330db7c23657bf09a3a2a9a8c4e83e38
Pablo Luque Salguero <pablols114@gmail.com>
2023-10-16 08:56:41 +0200

    Elimino latitud y longitud por distancia según #16

--- src/route.go
+++ src/route.go
@@ -17,27 +17,23 @@ func NewOpeningHours(day string, startTime string, endTime string) *OpeningHours
 // Definicion de la entidad Area de descanso
 type RestArea struct {
 	openingHours []OpeningHours
-	latitude     float64
-	longitude    float64
+	distance     uint16
 }
 
 func (r RestArea) OpeningHours() []OpeningHours {
 	return r.openingHours
 }
 
-func (r RestArea) Latitude() float64 {
-	return r.latitude
+func (r RestArea) Distance() uint16 {
+	return r.distance
 }
 
-func (r RestArea) Longitude() float64 {
-	return r.longitude
 }
 
 func NewRestArea(openingHours []OpeningHours, latitude float64, longitude float64) *RestArea {
 	return &RestArea{
 		openingHours: openingHours,
-		latitude:     latitude,
-		longitude:    longitude,
+		distance:     distance,
 	}
 }
 
